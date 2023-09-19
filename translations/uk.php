6f4eee15cb4a266513ee4182be0f3760006c8e9d
Yura Kyziv <kyzivyra.3@gmail.com>
2023-09-19 22:59:02 +0300

    Add ukrainian translations

---
+++ translations/uk.php
@@ -0,0 +1,10 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{wishlist}prestashop>wishlist_9f7b857dbd2834e6dbae6d8de2611789'] = 'Список бажань';
+$_MODULE['<{wishlist}prestashop>wishlist_e2fc90da03e73aec103793893b77024b'] = ' Модуль списку бажань.';
+$_MODULE['<{wishlist}prestashop>wishlist_876f23178c29dc2552c0b48bf23cd9bd'] = ' Ви впевнені, що хочете видалити?';
+$_MODULE['<{wishlist}prestashop>wishlist_0f40e8817b005044250943f57a21c5e7'] = ' Ім\'я не вказано';
+$_MODULE['<{wishlist}prestashop>product_page_wishlist_2d96bb66d8541a89620d3c158ceef42b'] = 'Додати до списку бажань';
+$_MODULE['<{wishlist}prestashop>wishlist_2d96bb66d8541a89620d3c158ceef42b'] = 'Додати до списку бажань';
