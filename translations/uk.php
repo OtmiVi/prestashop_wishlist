23051315db8e4187285edd16a4c18b75f2f902b1
Yura Kyziv <kyzivyra.3@gmail.com>
2023-09-21 14:45:50 +0300

    Update translations

--- translations/uk.php
+++ translations/uk.php
@@ -3,8 +3,13 @@
 global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{wishlist}prestashop>wishlist_9f7b857dbd2834e6dbae6d8de2611789'] = 'Список бажань';
-$_MODULE['<{wishlist}prestashop>wishlist_e2fc90da03e73aec103793893b77024b'] = ' Модуль списку бажань.';
-$_MODULE['<{wishlist}prestashop>wishlist_876f23178c29dc2552c0b48bf23cd9bd'] = ' Ви впевнені, що хочете видалити?';
-$_MODULE['<{wishlist}prestashop>wishlist_0f40e8817b005044250943f57a21c5e7'] = ' Ім\'я не вказано';
+$_MODULE['<{wishlist}prestashop>wishlist_e2fc90da03e73aec103793893b77024b'] = 'Модуль списку бажань.';
+$_MODULE['<{wishlist}prestashop>wishlist_876f23178c29dc2552c0b48bf23cd9bd'] = 'Ви впевнені, що хочете видалити?';
+$_MODULE['<{wishlist}prestashop>wishlist_0f40e8817b005044250943f57a21c5e7'] = 'Ім\'я не вказано';
+$_MODULE['<{wishlist}prestashop>list_9f7b857dbd2834e6dbae6d8de2611789'] = 'Список бажань';
+$_MODULE['<{wishlist}prestashop>customeraccount_9f7b857dbd2834e6dbae6d8de2611789'] = 'Список бажань';
+$_MODULE['<{wishlist}prestashop>list_5722cff8f8de1c39bec6a5b219c3de3e'] = 'У вас немає товарів у списку бажань';
+$_MODULE['<{wishlist}prestashop>list_c33b854973addbdabc7b874491807c73'] = ' Видалити зі списку бажань';
+$_MODULE['<{wishlist}prestashop>list_button_9f7b857dbd2834e6dbae6d8de2611789'] = 'Список бажань';
 $_MODULE['<{wishlist}prestashop>product_page_wishlist_2d96bb66d8541a89620d3c158ceef42b'] = 'Додати до списку бажань';
 $_MODULE['<{wishlist}prestashop>wishlist_2d96bb66d8541a89620d3c158ceef42b'] = 'Додати до списку бажань';
