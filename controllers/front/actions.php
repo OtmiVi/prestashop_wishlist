07fa11a5846b4f097170a9f5baf6c363f2d115b5
Yura Kyziv <kyzivyra.3@gmail.com>
2023-09-21 11:53:07 +0300

    Add remove action

--- controllers/front/actions.php
+++ controllers/front/actions.php
@@ -31,4 +31,31 @@ class WishlistActionsModuleFrontController extends ModuleFrontController
         exit;
     }
 
+    public function displayAjaxRemove()
+    {
+
+        $productId = (int)Tools::getValue('productId');
+        $customerId = $this->context->customer->id;
+
+        $sql = new DbQuery();
+        $sql->select('id_wishlist');
+        $sql->from('wishlist', 'w');
+        $sql->where('w.id_product = ' . $productId);
+        $sql->where('w.id_customer = ' . $customerId);
+        $sql = Db::getInstance()->executes($sql);
+
+        foreach ($sql as $item){
+            $wishList = new WishListEntity($item['id_wishlist']);
+            $wishList->delete();
+        }
+
+        $this->ajaxRender(
+            json_encode([
+                'success' => 1,
+                'message' => [1, 1],
+            ])
+        );
+        exit;
+    }
+
 }
